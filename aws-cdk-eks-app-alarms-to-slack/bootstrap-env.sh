2b96332d145ff7f62ccb98a0c6829c54dc5b0fa6
iamprakkie <gprakk.ie@gmail.com>
2023-02-09 21:56:28 -0700

    fixed bootstrap-env and cdk.json

--- aws-cdk-eks-app-alarms-to-slack/bootstrap-env.sh
+++ aws-cdk-eks-app-alarms-to-slack/bootstrap-env.sh
@@ -4,9 +4,9 @@
 set -e
 
 # setting environment variables
-export CAP_ACCOUNT_ID=$CAP_ACCOUNT_ID || $(aws sts get-caller-identity --query 'Account' --output text)
-export CAP_CLUSTER_REGION=$CAP_CLUSTER_REGION || "us-west-2"
-export CAP_CLUSTER_NAME=$CAP_CLUSTER_NAME || "demo-cluster"
+export CAP_ACCOUNT_ID=${CAP_ACCOUNT_ID:-$(aws sts get-caller-identity --query 'Account' --output text)}
+export CAP_CLUSTER_REGION=${CAP_CLUSTER_REGION:-"us-west-2"}
+export CAP_CLUSTER_NAME=${CAP_CLUSTER_NAME:-"demo-cluster"}
 
 echo -e "\n"
 echo "Following environment variables are set:"
