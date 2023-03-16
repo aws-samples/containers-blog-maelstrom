5e28b3098587d22918cfb48937995953c1b76516
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-03-16 14:17:26 +0000

    New Commit for Grafana Operator Blog

---
+++ grafana-operator-AMG/README.md
@@ -0,0 +1,4 @@
+# Using Amazon EKS Blueprints and Open Telemetry to capture errors from Kubernetes applications on Slack
+
+This repository contains code to demonstrate sending notifications to Slack channel from Amazon CloudWatch for application errors in Amazon EKS. Please clone this repository and follow steps mentioned in the blog.
+
---
+++ grafana-operator-AMG/bootstrap-env.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+NC='\033[0m'       # Text Reset
+R='\033[0;31m'          # Red
+G='\033[0;32m'        # Green
+Y='\033[0;33m'       # Yellow
+echo -e "${Y}"
+
+# checking environment variables
+
+if [ -z "${GO_ACCOUNT_ID}" ]; then
+    echo -e "${R}env variable CAP_ACCOUNT_ID not set${NC}"; exit 1
+fi
+
+if [ -z "${GO_CLUSTER_REGION}" ]; then
+    echo -e "${R}env variable CAP_CLUSTER_REGION not set${NC}"; exit 1
+fi
+
+if [ -z "${GO_CLUSTER_NAME}" ]; then
+    echo -e "${R}env variable CAP_CLUSTER_NAME not set${NC}"; exit 1
+fi
+
+curr_dir=${PWD}
+
+echo "Following environment variables will be used:"
+echo "GO_ACCOUNT_ID = "$GO_ACCOUNT_ID
+echo "GO_CLUSTER_REGION = "$GO_CLUSTER_REGION
+echo "GO_CLUSTER_NAME = "$GO_CLUSTER_NAME
+
+# bootstrapping CDK
+echo -e "\n"
+echo -e "bootstrapping CDK"
+npm install
+cdk bootstrap aws://${GO_ACCOUNT_ID}/${GO_CLUSTER_REGION}
+
