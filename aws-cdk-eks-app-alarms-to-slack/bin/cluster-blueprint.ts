f9bc4bed9ad660b248345f88cfdad4ae66c37d18
iamprakkie <gprakk.ie@gmail.com>
2023-04-11 15:43:20 -0600

    updated scripts

--- aws-cdk-eks-app-alarms-to-slack/bin/cluster-blueprint.ts
+++ aws-cdk-eks-app-alarms-to-slack/bin/cluster-blueprint.ts
@@ -53,13 +53,7 @@ const addOns: Array<blueprints.ClusterAddOn> = [
     new blueprints.addons.VpcCniAddOn(),
     new blueprints.addons.CoreDnsAddOn(),
     new blueprints.addons.KubeProxyAddOn(),
-    //new blueprints.addons.CertManagerAddOn(),
-    //new blueprints.addons.AdotCollectorAddOn(),
     new blueprints.addons.AwsForFluentBitAddOn(props)
-    //, new blueprints.addons.CloudWatchAdotAddOn({deploymentMode: blueprints.addons.cloudWatchDeploymentMode.DEPLOYMENT,
-    //    metricsNameSelectors: ['apiserver_request_.*', 'container_memory_.*', 'container_threads', 'otelcol_process_.*', 'ho11y*'],
-    //    podLabelRegex: 'frontend|downstream(.*)'}
-    //)
 ];
 
 
