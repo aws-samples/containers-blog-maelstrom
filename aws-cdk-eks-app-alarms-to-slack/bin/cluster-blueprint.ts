137204587e19e618f7b95a1072c23a70a64e2f45
iamprakkie <gprakk.ie@gmail.com>
2023-04-11 00:21:27 -0600

    updated metric filter

--- aws-cdk-eks-app-alarms-to-slack/bin/cluster-blueprint.ts
+++ aws-cdk-eks-app-alarms-to-slack/bin/cluster-blueprint.ts
@@ -12,8 +12,9 @@ const account = process.env.CAP_ACCOUNT_ID! || process.env.CDK_DEFAULT_ACCOUNT!;
 const region = process.env.CAP_CLUSTER_REGION! || process.env.CDK_DEFAULT_REGION!;
 const clusterName = process.env.CAP_CLUSTER_NAME!;
 
-const logGroupName = "/aws/eks/" + clusterName + "/pod-logs"
-const resource = "arn:aws:logs:" + region + ":" + account +":log-group:"+ logGroupName + ":*" 
+const logGroupName = "/aws/eks/fluentbit-cloudwatch/" + clusterName + "/workload/$kubernetes['namespace_name']"
+const resource = "arn:aws:logs:" + region + ":" + account +":log-group:"+ "/aws/eks/fluentbit-cloudwatch/" + clusterName + "/workload/*" + ":*" 
+
 
 const cwWritePolicy = new iam.PolicyStatement({
     actions: [
@@ -22,20 +23,27 @@ const cwWritePolicy = new iam.PolicyStatement({
         "logs:DescribeLogStreams",
         "logs:DescribeLogGroups",
         "logs:CreateLogStream",
-        "logs:CreateLogGroup"
+        "logs:CreateLogGroup",
+        "logs:PutRetentionPolicy"
     ],
     resources: [resource],
     effect: iam.Effect.ALLOW,
   });
 
+//for more info on values: https://github.com/aws/eks-charts/blob/master/stable/aws-for-fluent-bit/values.yaml
 const props: AwsForFluentBitAddOnProps = {
     iamPolicies: [cwWritePolicy],
     values: {
-      cloudWatch: {
+      cloudWatchLogs: {
         enabled: true,
         region: region,
-        logGroupName: logGroupName,
-      },
+        logGroupTemplate: logGroupName,
+        logStreamTemplate: "$kubernetes['pod_name'].$kubernetes['container_name']",
+        autoCreateGroup: true,
+        logFormat: "json/emf",
+        logKey: "log",
+        logRetentionDays:90,
+      },  
     },
   };
   
