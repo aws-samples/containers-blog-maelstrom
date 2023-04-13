e47562f1e891ae950842b3b93b50d0dacd41ef6c
nbbathul <nbbathul@amazon.com>
2023-04-12 20:09:59 -0500

    Changes to readME files and cleanup script

--- prefetch-data-to-EKSnodes/cleanup.sh
+++ prefetch-data-to-EKSnodes/cleanup.sh
@@ -16,19 +16,20 @@ eksctl delete nodegroup --cluster=$EDP_NAME --name=nodegroup
 
 echo ***** Deleting EKS Cluster *****
 
-eksctl delete cluster --name=$EDP_NAME
+eksctl delete cluster --name=$EDP_NAME --region $EDP_AWS_REGION
 
 # Delete State Manager Association
 
 echo ***** Deleting AWS Systems Manager State Manager Association *****
 
-association_id=$(aws ssm list-associations --association-filter-list key=Name,value=AWS-RunShellScript --query "Associations[0].AssociationId" --output text)
+association_id=$(aws ssm list-associations --association-filter-list key=Name,value=AWS-RunShellScript --query "Associations[0].AssociationId" --region $EDP_AWS_REGION --output text)
 
 if [ -z "$association_id" ]; then
   echo "No association found for document $document_name."
   exit 1
 fi
-aws ssm delete-association --association-id $association_id
+
+aws ssm delete-association --association-id $association_id --region $EDP_AWS_REGION
 
 # Delete EventBridge Rule along with Target attached to it
 
@@ -36,28 +37,36 @@ aws ssm delete-association --association-id $association_id
 
 echo ***** Deleting EventBridge Target *****
 
-TARGETS=$(aws events list-targets-by-rule --rule $EDP_NAME | jq -r '.Targets[].Id')
+TARGETS=$(aws events list-targets-by-rule --rule $EDP_NAME --region $EDP_AWS_REGION | jq -r '.Targets[].Id')
 
 # Remove each target from the rule
+
 for target in $TARGETS; do
-  aws events remove-targets --rule $EDP_NAME --ids $target
+  aws events remove-targets --rule $EDP_NAME --ids $target --region $EDP_AWS_REGION
 done
 
 # Delete the rule
 
 echo ***** Deleting EventBridge Rule *****
-aws events delete-rule --name $EDP_NAME
+
+aws events delete-rule --name $EDP_NAME --region $EDP_AWS_REGION
 
 # Delete the role we created for EventBridge Rule
 
 echo ***** Deleting Role and Policy we created for EventBridge Rule *****
+
 aws iam delete-role-policy --role-name ${EDP_NAME}-role --policy-name ${EDP_NAME}-policy
 aws iam delete-role --role-name ${EDP_NAME}-role
 
 # Delete ECR Repo
-aws ecr delete-repository --repository-name $EDP_NAME --force
 
-# Delete Docker Image
+echo ***** Deleting ECR Repo *****
+
+aws ecr delete-repository --repository-name $EDP_NAME --force --region $EDP_AWS_REGION
+
+# Delete Docker Image and files folder that we created in order to create large Docker Image
+
+echo ***** Deleting Docker Image and Files Folder *****
 
 docker rmi $EDP_NAME
 docker rmi $EDP_AWS_ACCOUNT.dkr.ecr.$EDP_AWS_REGION.amazonaws.com/$EDP_NAME
