da61f87f569f6168e75f2d0d939f180325b82f79
bingjiao <bingjiao@amazon.com>
2023-05-06 14:10:26 +0800

    Add topology aware hints

---
+++ topology-aware-hints/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "num_nodes_az1" {
+  type    = number
+  default = 1 
+}
+
+variable "num_nodes_az2" {
+  type    = number
+  default = 1 
+}
+
+variable "num_nodes_az3" {
+  type    = number
+  default = 1 
+}
