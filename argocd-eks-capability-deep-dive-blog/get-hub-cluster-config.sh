c20f1f1af338a51b024ba5347d318f839c058d5b
Sébastien Allamand <sallaman@amazon.com>
2025-12-12 11:23:13 +0100

    add script to get conf
    Signed-off-by: Sébastien Allamand <sallaman@amazon.com>

---
+++ argocd-eks-capability-deep-dive-blog/get-hub-cluster-config.sh
@@ -0,0 +1,22 @@
+# !/bin/bash
+
+aws eks update-kubeconfig --name hub-cluster --region $AWS_REGION --alias hub-cluster
+# Store the cluster ARN for later use
+export AWS_EKS_HUB_CLUSTER_ARN="arn:aws:eks:$AWS_REGION:$AWS_ACCOUNT_ID:cluster/hub-cluster"
+# Store the Argo CD capability IAM Role ARN
+export AWS_IAM_ROLE_ARGO_CD_ARN=$(aws eks describe-capability \
+  --cluster-name hub-cluster \
+  --capability-name argocd \
+  --region $AWS_REGION \
+  --query 'capability.roleArn' \
+  --output text)
+# Store the Argo CD server URL
+export ARGO_CD_URL=$(aws eks describe-capability \
+  --cluster-name hub-cluster \
+  --capability-name argocd \
+  --region $AWS_REGION \
+  --query 'capability.configuration.argoCd.serverUrl' \
+  --output text)
+echo "Argo CD Server URL: $ARGO_CD_URL"
+
+
